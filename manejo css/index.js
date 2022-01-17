95b9fe61b02d8018a441d3ddcc2ffe5674447451
UlisesJE <ujuareze1700@alumno.ipn.mx>
2022-01-16 21:20:30 -0600

    manipulación de estilos con js

---
+++ manejo css/index.js
@@ -0,0 +1,7 @@
+document.querySelector("h2").style.color="blue";
+
+document.getElementsByName("btn")[0].style.backgroundColor="yellow";
+
+document.querySelector("button#oculto").classList.add("invisible");
+
+document.querySelectorAll("h4")[0].classList.add("grande");
