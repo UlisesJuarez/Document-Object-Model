52a0ac140e37cdbf88e2b05f2f3cb40feaa6f753
UlisesJE <ujuareze1700@alumno.ipn.mx>
2022-01-16 19:43:55 -0600

    uso de diferentes selectores del modelo dom

---
+++ selectores/index.js
@@ -0,0 +1,10 @@
+document.getElementsByTagName("li")[1].style.color="orange";
+
+document.getElementsByClassName("btn")[0].style.background="gray";
+
+document.getElementById("third").style.color="red"
+document.getElementById("third").innerHTML="Me modifique con un selector de id"
+
+document.querySelector("h2").innerHTML="Good bye everyone!!!";
+
+document.querySelector("li a").innerHTML="Acceder a Google";
